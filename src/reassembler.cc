a185b3e4e0e50d58d2646db6b4260e49fdb14960
warrior <1986411713@qq.com>
2024-06-25 22:54:42 +0800

    基本完成代码的修改，还有is_last_substring的情况需要添加

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,65 +4,30 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  if (writer().bytes_pushed() > first_index){   //出现了重复发送的情况
-    return;
-  }
-
-  if (writer().available_capacity() - buffersize_ >= data.size()){    //bytestream的可用空间足够
-    if (writer().bytes_pushed() == first_index){   //数据按序到达的情况
+  if (first_index <= writer().bytes_pushed() && (first_index + data.size()) > writer().bytes_pushed()){
+    if (data.size() <= writer().available_capacity()){
+      data = data.substr(writer().bytes_pushed());   
       output_.writer().push(data);
-      if (is_last_substring){    //顺序到达的最后一个子串，关闭writer的连接
-        setBuffersize();
-        output_.writer().close();
-        return;
-      }
-      bool flag = false;
-      while (true){
-        if (buffer.empty())   //缓冲区为空，则直接退出循环
-          break;
-        for (auto& iter: buffer){
-          if (iter.first == writer().bytes_pushed()){    //删除buffer中的满足条件的string
-            output_.writer().push(iter.second);
-            buffer.erase(iter.first);
-            flag = true;
-            break;
-          }
-        }
-        if (!flag)
-          break;
-      }
-      setBuffersize();   //更新buffersize_的值
-      if (is_last && buffer.empty())
-        output_.writer().close();
-    }else{   //数据错序到达的情况
-      buffer.insert({first_index, data});
-      setBuffersize();   //更新buffersize的值
-      if (is_last_substring)
-        is_last = true;
-    }
-  }else{   //bytestream的可用空间不够
-    auto datalen = writer().available_capacity() - buffersize_;
-    data = data.substr(0, datalen);
-    if (writer().bytes_pushed() == first_index){   //数据按序到达的情况
+    }else{
+      data = data.substr(writer().bytes_pushed(), writer().available_capacity());
       output_.writer().push(data);
-      bool flag = false;
-      while (true){
-        for (auto& iter: buffer){
-          if (iter.first == writer().bytes_pushed()){    //删除buffer中的满足条件的string
-            output_.writer().push(iter.second);
-            buffer.erase(iter.first);
-            flag = true;
-            break;
-          }
-        }
-        if (!flag)
-          break;
-      }
-      setBuffersize();   //更新buffersize_的值
-    }else{   //数据错序到达的情况
-      buffer.insert({first_index, data});
-      setBuffersize();   //更新buffersize的值
     }
+    if (!buffer.empty()){
+      for (auto& iter: buffer){   //取出缓冲区中的字符串
+        if (iter.first == writer().bytes_pushed()){
+          output_.writer().push(iter.second);   
+          setBuffersize();
+          break;
+        }
+      }
+    }
+  }else if (first_index > writer().bytes_pushed()){
+    if (data.size() <= writer().available_capacity()){
+      buffer[first_index] = data; 
+    }else{
+      buffer[first_index] = data.substr(0, writer().available_capacity());
+    }
+    setBuffersize();
   }
 }
 
