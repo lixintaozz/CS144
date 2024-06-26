e1d9953097dda1591d3284e7d5047b9d76f18c9e
warrior <1986411713@qq.com>
2024-06-26 09:14:42 +0800

    添加空字符串的处理逻辑

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,10 +4,21 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
+  if (data.empty()){
+    if (is_last_substring)
+      output_.writer().close();
+    return;
+  }
+
+
   if (first_index <= writer().bytes_pushed() && (first_index + data.size()) > writer().bytes_pushed()){
     if (data.size() <= writer().available_capacity()){
       data = data.substr(writer().bytes_pushed());   
       output_.writer().push(data);
+      if (is_last_substring) {
+        output_.writer().close();
+        return;
+      }
     }else{
       data = data.substr(writer().bytes_pushed(), writer().available_capacity());
       output_.writer().push(data);
@@ -17,10 +28,13 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
         if (iter.first == writer().bytes_pushed()){
           output_.writer().push(iter.second);   
           setBuffersize();
+          buffer.erase(iter.first);
           break;
         }
       }
     }
+    if (buffer.empty() && is_last)
+      output_.writer().close();
   }else if (first_index > writer().bytes_pushed()){
     if (data.size() <= writer().available_capacity()){
       buffer[first_index] = data; 
@@ -28,6 +42,8 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
       buffer[first_index] = data.substr(0, writer().available_capacity());
     }
     setBuffersize();
+    if (is_last_substring)
+      is_last = true;
   }
 }
 
