dff42fc49a466db1ed6753b703afb40db152d664
warrior <1986411713@qq.com>
2024-07-01 21:03:37 +0800

    19/29 Test #37: wrapping_integers_extra ...... Passed  优化代码并添加注释

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -20,15 +20,19 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   //将32位的相对序号转换为64位的绝对序号
   checkpoint += zero_point.raw_value_;
   uint64_t high32 = checkpoint & 0xffffffff00000000;
+
+  //序号恰好落在checkpoint所在区间
   uint64_t res = high32 | raw_value_;
-
-  if (res < zero_point.raw_value_){
-    return res + (1UL << 32) - zero_point.raw_value_;
-  }
-
+  //序号落在checkpoint所在区间的前一个区间
   uint64_t resl = res - (1UL << 32);
+  //序号落在checkpoint所在区间的后一个区间
   uint64_t resr = res + (1UL << 32);
 
+  //如果序号落在zero_point之前，则只需要考虑resr的情况
+  if (res < zero_point.raw_value_)
+    return resr - zero_point.raw_value_;
+
+  //否则需要考虑三种区间的情况
   uint64_t dl = dist(resl, checkpoint);
   uint64_t dm = dist(res, checkpoint);
   uint64_t dr = dist(resr, checkpoint);
