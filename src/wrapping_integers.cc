c742e962b43016942cb6c3ea40df0be128dfbccd
warrior <1986411713@qq.com>
2024-07-01 20:37:08 +0800

    19/29 Test #37: wrapping_integers_extra ..........   Passed

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,9 +1,10 @@
 #include "wrapping_integers.hh"
+#include <algorithm>
 
 using namespace std;
 
 
-uint64_t distance(uint64_t a, uint64_t b) {
+uint64_t dist(uint64_t a, uint64_t b) {
   return (a > b) ? (a - b) : (b - a);
 }
 
@@ -17,7 +18,28 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   //将32位的相对序号转换为64位的绝对序号
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  checkpoint += zero_point.raw_value_;
+  uint64_t high32 = checkpoint & 0xffffffff00000000;
+  uint64_t res = high32 | raw_value_;
+
+  if (res < zero_point.raw_value_){
+    return res + (1UL << 32) - zero_point.raw_value_;
+  }
+
+  uint64_t resl = res - (1UL << 32);
+  uint64_t resr = res + (1UL << 32);
+
+  uint64_t dl = dist(resl, checkpoint);
+  uint64_t dm = dist(res, checkpoint);
+  uint64_t dr = dist(resr, checkpoint);
+
+  uint64_t dmin = min({dl, dm, dr});
+
+  if (dmin == dl){
+    return resl - zero_point.raw_value_;
+  }else if (dmin == dm){
+    return res - zero_point.raw_value_;
+  }else{
+    return resr - zero_point.raw_value_;
+  }
 }
