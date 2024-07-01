661c4c8b407f5290e900281867f440230c5b6191
warrior <1986411713@qq.com>
2024-07-01 11:29:03 +0800

    17/29 Test #33: wrapping_integers_unwrap .........***Timeout 时间复杂度太高，需要思考更快的算法

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -15,18 +15,25 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
   //将32的相对序号转换为64位的绝对序号
 
-  //先将checkpoint转换为其对应的32位序号
-  auto check32 = wrap(checkpoint, zero_point);
-  //计算wrap32的表示范围
-  uint64_t l = checkpoint - check32.raw_value_;
-  uint64_t r = checkpoint + num - 1 - check32.raw_value_;
+  //定义l,r分别向左右遍历
+  uint64_t l = checkpoint;
+  uint64_t r = checkpoint;
 
-  //while遍历寻找
-  while (l <= r){
-    if (wrap(l, zero_point) == *this){
-      break;
+  //考虑checkpoint为0的情况
+  if (checkpoint == 0){
+    while (!(wrap(r, zero_point) == *this)){
+      ++ r;
     }
-    ++ l;
+    return r;
   }
-  return l;
+
+  while (!(wrap(l, zero_point) == *this) && !( wrap(r, zero_point ) == *this)) {
+    if (l > 0)
+      -- l;
+    ++ r;
+  }
+  if (wrap(l, zero_point) == *this)
+    return l;
+  else
+    return r;
 }
